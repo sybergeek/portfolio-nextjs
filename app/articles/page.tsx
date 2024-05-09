cccd5567a1085901e0e9b341250c1a30ae18f557
sybergeek <abhishek13dgp5@gmail.com>
2024-05-10 00:24:15 +0530

    Add sections in Articles page

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,9 +1,147 @@
 "use client";
 
-export default function About() {
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import AnimatedText from "../components/AnimatedText";
+import Layout from "../components/Layout";
+import article1 from "../../public/images/articles/pagination component in reactjs.jpg";
+import article2 from "../../public/images/articles/create loading screen in react js.jpg";
+import { motion, useMotionValue } from "framer-motion";
+import article3 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
+import { useRef } from "react";
+
+const FramerImage = motion(Image);
+
+const MovingImage = ({title, img, link}) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  function handleMouse(event) {
+    imgRef.current.style.display = "inline-block";
+    x.set(event.pageX);
+    y.set(-10);
+  }
+
+  function handleMouseLeave(event) {
+    imgRef.current.style.display = "none";
+    x.set(0);
+    y.set(0);
+  }
+
+  return (
+    <Link href={link} target="_blank"
+    onMouseMove={handleMouse}
+    onMouseLeave={handleMouseLeave}
+    >
+      <h2 className="capitalize text-xl font-semibold hover:underline">{title}</h2>
+      <FramerImage
+      style={{ x: x, y: y }}
+      whileInView={{ opacity: 1, transition: { duration: 0.2 } }}
+      initial={{ opacity: 0 }}
+      ref={imgRef} src={img} alt={title} className="z-10 w-96 h-auto hidden absolute rounded-lg" />
+    </Link>
+  )
+}
+
+const Article = ({img, title, date, link}) => {
+  return <motion.li 
+  initial={{ y: 200 }}
+  whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
+  viewport={{ once: true }}
+  className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between
+  bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4">
+    <MovingImage title={title} img={img} link={link} />
+    <span  className="text-primary font-semibold pl-4">{date}</span>
+  </motion.li>
+}
+
+const FeaturedArticle = ({img, title, time, summary, link}) => {
+  return <li className="col-span-1 w-full p-4 bg-light border border-solid
+  border-dark rounded-2xl relative">
+    <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[102.5%]
+      rounded-[2rem] bg-dark rounded-br-3xl" />
+    <Link href={link} target="_blank"
+      className="w-full inline-block cursor-pointer overflow-hidden rounded-lg"
+      >
+        <FramerImage src={img} alt={title} className="w-full h-auto"
+        whileHover={{ scale: 1.05 }}
+        transition={{ duration: 0.2 }}
+        />
+      </Link>
+      <Link href={link} target="_blank">
+        <h2 className="capitalize text-2xl font-bold my-2 hover:underline mt-4">{title}</h2>
+      </Link>
+      <p className="text-sm mb-2">{summary}</p>
+      <span className="text-primary font-semibold">{time}</span>
+  </li>
+}
+
+export default function Articles() {
   return (
     <>
-      Articles
+      <Head>
+        <title>Abhishek Sen | Articles Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center mb-16 overflow-hidden">
+        <Layout className="pt-16">
+          <AnimatedText text="Words Can Change The World!" className="mb-16" />
+          <ul className="grid grid-cols-2 gap-16">
+            <FeaturedArticle
+            img={article1}
+            title="Build A Custom Pagination Component In Reactjs From Scratch"
+            time="9 min read"
+            summary="Learn how to build a custom pagination component in ReactJS from scratch. 
+            Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+            link="/"
+            />
+            <FeaturedArticle
+            img={article2}
+            title="Creating Stunning Loading Screens In React: Build 3 Types Of Loading Screens"
+            time="10 min read"
+            summary="Learn how to create stunning loading screens in React with 3 different methods. 
+            Discover how to use React-Loading, React-Lottie & build a custom loading screen. 
+            Improve the user experience."
+            link="/"
+            />
+          </ul>
+          <h2 className="font-bold text-4xl w-full text-center my-16 mt-32">All Articles</h2>
+          <ul>
+            <Article
+            title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            img={article3}
+            date="March 22, 2023"
+            link="/"
+            />
+            <Article
+            title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            img={article3}
+            date="March 22, 2023"
+            link="/"
+            />
+            <Article
+            title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            img={article3}
+            date="March 22, 2023"
+            link="/"
+            />
+            <Article
+            title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            img={article3}
+            date="March 22, 2023"
+            link="/"
+            />
+            <Article
+            title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+            img={article3}
+            date="March 22, 2023"
+            link="/"
+            />
+          </ul>
+        </Layout>
+      </main>
     </>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,6 +7,9 @@ import AnimatedText from "../components/AnimatedText";
 import { GithubIcon } from "../components/Icons";
 import Layout from "../components/Layout";
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import { motion } from "framer-motion";
+
+const FramerImage = motion(Image);
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
   return <article className="w-full flex items-center justify-between
@@ -16,7 +19,10 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
       <Link href={link} target="_blank"
       className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage src={img} alt={title} className="w-full h-auto"
+        whileHover={{ scale: 1.05 }}
+        transition={{ duration: 0.2 }}
+        />
       </Link>
       <div className="w-1/2 flex flex-col items-start justify-between pl-6">
         <span className="text-primary font-medium text-xl">{type}</span>
@@ -47,7 +53,10 @@ const Project = ({type, title, img, link, github}) => {
       <Link href={link} target="_blank"
       className="w-full cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage src={img} alt={title} className="w-full h-auto"
+        whileHover={{ scale: 1.05 }}
+        transition={{ duration: 0.2 }}
+        />
       </Link>
       <div className="w-full flex flex-col items-start justify-between mt-4">
         <span className="text-primary font-medium text-xl">{type}</span>
